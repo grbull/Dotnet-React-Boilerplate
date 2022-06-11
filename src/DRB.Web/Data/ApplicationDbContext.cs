a199a7f894c45e278bd03662efc3760d0fe19540
Graham Bull <graham.bull@gmail.com>
2022-06-11 15:48:05 +0100

    added application db context

---
+++ src/DRB.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using DRB.Web.Data.Entities;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DRB.Web.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+    }
+}
