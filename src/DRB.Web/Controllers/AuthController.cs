cc451052f5c18c83a5a52f03c9997ddb19d54b42
Graham Bull <graham.bull@gmail.com>
2022-06-12 21:51:11 +0100

    implemented auth controller register method

--- src/DRB.Web/Controllers/AuthController.cs
+++ src/DRB.Web/Controllers/AuthController.cs
@@ -1,3 +1,6 @@
+using DRB.Web.Data.Entities;
+using DRB.Web.DTOs;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DRB.Web.Controllers;
@@ -7,9 +10,32 @@ namespace DRB.Web.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly ILogger<AuthController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public AuthController(ILogger<AuthController> logger)
+    public AuthController(ILogger<AuthController> logger, UserManager<ApplicationUser> userManager)
     {
         this._logger = logger;
+        this._userManager = userManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] UserRegisterDTO registerDto)
+    {
+        var user = await this._userManager.FindByNameAsync(registerDto.UserName);
+        if (user is null)
+        {
+            // TODO: Return a more appropriate response
+            return this.Ok();
+        }
+        
+        user = new ApplicationUser()
+        {
+            Id = new Guid().ToString(),
+            UserName = registerDto.UserName,
+            Email = registerDto.Email
+        };
+        await this._userManager.CreateAsync(user, registerDto.Password);
+
+        return this.Ok();
     }
 }
