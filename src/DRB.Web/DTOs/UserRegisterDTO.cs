026ece64db318c5511498e8c2dcb368733f72beb
Graham Bull <graham.bull@gmail.com>
2022-06-12 21:50:52 +0100

    added user registration dto

---
+++ src/DRB.Web/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace DRB.Web.DTOs;
+
+public class UserRegisterDTO
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
